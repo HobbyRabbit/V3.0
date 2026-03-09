f7c1a58b57e93a448da1d58dcca89775aad0af9c
HobbyRabbit <stephencewood@gmail.com>
2026-03-09 19:07:14 -0400

    Update __init__.py

--- custom_components/ac_infinity/__init__.py
+++ custom_components/ac_infinity/__init__.py
@@ -1,31 +1,42 @@
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
 from .const import DOMAIN
 from .coordinator import ACInfinityCoordinator
 
-PLATFORMS = ["fan", "switch", "sensor"]
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
-    coordinator = ACInfinityCoordinator(hass, entry)
+    coordinator = ACInfinityCoordinator(
+        hass,
+        entry.data["address"]
+    )
 
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["fan", "sensor"]
+    )
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
 
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        ["fan", "sensor"]
+    )
 
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
