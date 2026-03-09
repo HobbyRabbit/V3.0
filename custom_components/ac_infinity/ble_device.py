7c1bf50b27888c3d04d5f2c363f946fd8416a06d
HobbyRabbit <stephencewood@gmail.com>
2026-03-09 12:59:54 -0400

    Update ble_device.py

--- custom_components/ac_infinity/ble_device.py
+++ custom_components/ac_infinity/ble_device.py
@@ -3,7 +3,7 @@ import logging
 
 from bleak import BleakClient
 
-from .const import WRITE_UUID, NOTIFY_UUID
+from .const import WRITE_UUID, NOTIFY_UUID, BLE_TIMEOUT
 from .protocol import build_packet, parse_state
 
 _LOGGER = logging.getLogger(__name__)
@@ -14,14 +14,14 @@ class ACInfinityBLE:
     def __init__(self, address):
 
         self.address = address
-
         self.client = BleakClient(address)
 
         self.temperature = None
         self.humidity = None
         self.ports = {}
 
-        self._event = asyncio.Event()
+        self._response_event = asyncio.Event()
+        self._command_lock = asyncio.Lock()
 
     async def connect(self):
 
@@ -32,7 +32,7 @@ class ACInfinityBLE:
 
         await self.client.start_notify(
             NOTIFY_UUID,
-            self._notification_handler
+            self._notification_handler,
         )
 
     async def disconnect(self):
@@ -42,7 +42,10 @@ class ACInfinityBLE:
 
     def _notification_handler(self, sender, data):
 
-        if data[0] != 0xAA:
+        if len(data) < 5:
+            return
+
+        if data[0] != 0xAA or data[1] != 0x55:
             return
 
         cmd = data[3]
@@ -55,35 +58,44 @@ class ACInfinityBLE:
             self.humidity = hum
             self.ports = ports
 
-            self._event.set()
+            self._response_event.set()
+
+    async def _send_command(self, packet):
+
+        async with self._command_lock:
+
+            await self.client.write_gatt_char(WRITE_UUID, packet)
 
     async def request_state(self):
 
         packet = build_packet(0x10, [])
 
-        self._event.clear()
+        self._response_event.clear()
 
-        await self.client.write_gatt_char(WRITE_UUID, packet)
+        await self._send_command(packet)
 
         try:
-            await asyncio.wait_for(self._event.wait(), 5)
+            await asyncio.wait_for(
+                self._response_event.wait(),
+                BLE_TIMEOUT,
+            )
         except asyncio.TimeoutError:
-            _LOGGER.warning("AC Infinity controller timeout")
+            _LOGGER.warning("AC Infinity BLE response timeout")
 
         return {
             "temperature": self.temperature,
             "humidity": self.humidity,
-            "ports": self.ports
+            "ports": self.ports,
         }
 
     async def set_speed(self, port, speed):
 
         packet = build_packet(0x11, [port, speed])
 
-        await self.client.write_gatt_char(WRITE_UUID, packet)
+        await self._send_command(packet)
 
     async def set_power(self, port, state):
 
         packet = build_packet(0x12, [port, 1 if state else 0])
 
-        await self.client.write_gatt_char(WRITE_UUID, packet)
+        await self._send_command(packet)
