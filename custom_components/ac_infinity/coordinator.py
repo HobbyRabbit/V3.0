966fc61f128fc3e6c4e51286b832b9e1d1b0787f
HobbyRabbit <stephencewood@gmail.com>
2026-03-09 19:07:53 -0400

    Update coordinator.py

--- custom_components/ac_infinity/coordinator.py
+++ custom_components/ac_infinity/coordinator.py
@@ -1,45 +1,33 @@
-import logging
 from datetime import timedelta
+import logging
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.core import HomeAssistant
 
+from .const import UPDATE_INTERVAL
 from .ble_device import ACInfinityBLE
-from .const import DOMAIN, SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class ACInfinityCoordinator(DataUpdateCoordinator):
 
-    def __init__(self, hass, entry):
+    def __init__(self, hass: HomeAssistant, address: str):
 
-        self.hass = hass
-
-        self.address = entry.data["address"]
-
-        self.ble = ACInfinityBLE(self.address)
+        self.ble = ACInfinityBLE(address)
 
         super().__init__(
             hass,
             _LOGGER,
-            name=DOMAIN,
-            update_interval=timedelta(seconds=SCAN_INTERVAL),
+            name="ac_infinity",
+            update_interval=timedelta(seconds=UPDATE_INTERVAL),
         )
 
     async def _async_update_data(self):
 
-        await self.ble.connect()
+        if not self.ble.connected:
+            await self.ble.connect()
 
-        return await self.ble.request_state()
+        data = await self.ble.get_status()
 
-    async def set_port_speed(self, port, speed):
-
-        await self.ble.set_speed(port, speed)
-
-        await self.async_request_refresh()
-
-    async def set_port_power(self, port, state):
-
-        await self.ble.set_power(port, state)
-
-        await self.async_request_refresh()
+        return data
