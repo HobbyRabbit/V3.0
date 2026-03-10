27e5a3172aa7dc41d6b15f22a96830add040d7c1
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 11:36:56 -0400

    Update switch.py

--- custom_components/ac_infinity/switch.py
+++ custom_components/ac_infinity/switch.py
@@ -1,39 +1,39 @@
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, PORT_COUNT
+from .const import DOMAIN
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
 
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    switches = []
+    entities = []
 
-    for port in range(1, PORT_COUNT + 1):
+    for port in range(1, 9):
+        entities.append(
+            ACInfinityPortSwitch(coordinator, port)
+        )
 
-        switches.append(ACInfinitySwitch(coordinator, entry.entry_id, port))
-
-    async_add_entities(switches)
+    async_add_entities(entities)
 
 
-class ACInfinitySwitch(CoordinatorEntity, SwitchEntity):
+class ACInfinityPortSwitch(CoordinatorEntity, SwitchEntity):
 
-    def __init__(self, coordinator, entry_id, port):
+    def __init__(self, coordinator, port):
 
         super().__init__(coordinator)
 
         self.port = port
-
-        self._attr_unique_id = f"{entry_id}_power_{port}"
-        self._attr_name = f"AC Infinity Port {port} Power"
+        self._attr_name = f"AC Infinity Port {port}"
+        self._attr_unique_id = f"{coordinator.mac}_port_{port}"
 
     @property
     def is_on(self):
-        return self.coordinator.data["ports"].get(self.port, 0) > 0
+        return self.coordinator.data["ports"][self.port - 1]
 
     async def async_turn_on(self, **kwargs):
-        await self.coordinator.set_port_power(self.port, True)
+        await self.coordinator.set_port(self.port, True)
 
     async def async_turn_off(self, **kwargs):
-        await self.coordinator.set_port_power(self.port, False)
+        await self.coordinator.set_port(self.port, False)
