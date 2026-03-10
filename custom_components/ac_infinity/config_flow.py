a8b2d7637ebcc5977e72d3f37b6ce050413d6374
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 11:35:48 -0400

    Update config_flow.py

--- custom_components/ac_infinity/config_flow.py
+++ custom_components/ac_infinity/config_flow.py
@@ -1,26 +1,35 @@
 import voluptuous as vol
-
 from homeassistant import config_entries
-
+from homeassistant.components.bluetooth import async_discovered_service_info
 from .const import DOMAIN
 
 
-class ACInfinityFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ACInfinityConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
 
+        devices = async_discovered_service_info(self.hass)
+
+        choices = {
+            d.address: f"{d.name} ({d.address})"
+            for d in devices
+            if "AC Infinity" in (d.name or "")
+        }
+
         if user_input is not None:
 
-            return self.async_create_entry(
-                title="AC Infinity Controller",
-                data=user_input
-            )
+            mac = user_input["mac"]
 
-        schema = vol.Schema({
-            vol.Required("address"): str
-        })
+            return self.async_create_entry(
+                title=mac,
+                data={"mac": mac},
+            )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema
+            data_schema=vol.Schema(
+                {
+                    vol.Required("mac"): vol.In(choices)
+                }
+            ),
         )
