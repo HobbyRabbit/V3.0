497bb671ae648e1d592379b9a462e90ba24712c5
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 11:37:46 -0400

    Update fan.py

--- custom_components/ac_infinity/fan.py
+++ custom_components/ac_infinity/fan.py
@@ -1,6 +1,5 @@
-from homeassistant.components.fan import FanEntity
+from homeassistant.components.fan import FanEntity, FanEntityFeature
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
 from .const import DOMAIN
 
 
@@ -13,13 +12,22 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
 class ACInfinityFan(CoordinatorEntity, FanEntity):
 
+    _attr_supported_features = FanEntityFeature.SET_SPEED
+    _attr_speed_count = 10
+
     def __init__(self, coordinator):
 
         super().__init__(coordinator)
 
         self._attr_name = "AC Infinity Fan"
+        self._attr_unique_id = f"{coordinator.mac}_fan"
 
     @property
     def percentage(self):
+        return self.coordinator.data["fan_speed"] * 10
 
-        return self.coordinator.data.get("speed", 0) * 10
+    async def async_set_percentage(self, percentage):
+
+        speed = int(percentage / 10)
+
+        await self.coordinator.set_speed(speed)
