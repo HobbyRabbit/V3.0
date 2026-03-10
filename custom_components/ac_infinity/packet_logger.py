0265bb85610d856911ab3369235adc6ffc0014eb
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 15:23:58 -0400

    Update packet_logger.py

--- custom_components/ac_infinity/packet_logger.py
+++ custom_components/ac_infinity/packet_logger.py
@@ -1,9 +1,25 @@
 import csv
+import os
 import time
+
 from .const import CSV_LOG
 
 
 class PacketLogger:
+
+    def __init__(self):
+
+        if not os.path.exists(CSV_LOG):
+
+            with open(CSV_LOG, "w", newline="") as f:
+
+                writer = csv.writer(f)
+
+                writer.writerow([
+                    "timestamp",
+                    "direction",
+                    "packet"
+                ])
 
     def log(self, direction, data):
 
