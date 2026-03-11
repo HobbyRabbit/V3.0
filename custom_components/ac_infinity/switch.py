259e9b312c349d6a08c87eaf10d17850a3250d21
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 21:29:42 -0400

    Update switch.py

--- custom_components/ac_infinity/switch.py
+++ custom_components/ac_infinity/switch.py
@@ -1,5 +1,5 @@
+
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
@@ -8,32 +8,26 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    entities = []
+    switches = []
 
     for port in range(1, 9):
-        entities.append(
+
+        switches.append(
             ACInfinityPortSwitch(coordinator, port)
         )
 
-    async_add_entities(entities)
+    async_add_entities(switches)
 
 
-class ACInfinityPortSwitch(CoordinatorEntity, SwitchEntity):
+class ACInfinityPortSwitch(SwitchEntity):
 
     def __init__(self, coordinator, port):
 
-        super().__init__(coordinator)
-
+        self.coordinator = coordinator
         self.port = port
         self._attr_name = f"AC Infinity Port {port}"
-        self._attr_unique_id = f"{coordinator.mac}_port_{port}"
 
     @property
     def is_on(self):
-        return self.coordinator.data["ports"][self.port - 1]
 
-    async def async_turn_on(self, **kwargs):
-        await self.coordinator.set_port(self.port, True)
-
-    async def async_turn_off(self, **kwargs):
-        await self.coordinator.set_port(self.port, False)
+        return False
