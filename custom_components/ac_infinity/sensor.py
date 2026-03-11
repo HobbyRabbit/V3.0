dbfb86663e7411ca25d7e07acc92d438ca9246cc
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 21:30:13 -0400

    Update sensor.py

--- custom_components/ac_infinity/sensor.py
+++ custom_components/ac_infinity/sensor.py
@@ -1,5 +1,5 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
 from .const import DOMAIN
 
 
@@ -7,39 +7,25 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    async_add_entities(
-        [
-            ACInfinityTemp(coordinator),
-            ACInfinityHumidity(coordinator),
-        ]
-    )
+    sensors = [
+        ACInfinityTemp(coordinator),
+        ACInfinityHumidity(coordinator),
+    ]
+
+    async_add_entities(sensors)
 
 
-class ACInfinityTemp(CoordinatorEntity, SensorEntity):
+class ACInfinityTemp(SensorEntity):
 
     def __init__(self, coordinator):
 
-        super().__init__(coordinator)
-
+        self.coordinator = coordinator
         self._attr_name = "AC Infinity Temperature"
-        self._attr_unique_id = f"{coordinator.mac}_temp"
-        self._attr_native_unit_of_measurement = "°C"
-
-    @property
-    def native_value(self):
-        return self.coordinator.data["temperature"]
 
 
-class ACInfinityHumidity(CoordinatorEntity, SensorEntity):
+class ACInfinityHumidity(SensorEntity):
 
     def __init__(self, coordinator):
 
-        super().__init__(coordinator)
-
+        self.coordinator = coordinator
         self._attr_name = "AC Infinity Humidity"
-        self._attr_unique_id = f"{coordinator.mac}_humidity"
-        self._attr_native_unit_of_measurement = "%"
-
-    @property
-    def native_value(self):
-        return self.coordinator.data["humidity"]
