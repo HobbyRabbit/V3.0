edff1d2b8f9500e7f6a3029b057aac475666909b
HobbyRabbit <stephencewood@gmail.com>
2026-03-10 21:26:26 -0400

    Update config_flow.py

--- custom_components/ac_infinity/config_flow.py
+++ custom_components/ac_infinity/config_flow.py
@@ -1,55 +1,39 @@
 from __future__ import annotations
 
-import voluptuous as vol
-
 from homeassistant import config_entries
+from homeassistant.components.bluetooth import BluetoothServiceInfoBleak
 from homeassistant.const import CONF_MAC
-from homeassistant.core import callback
 
 from .const import DOMAIN
 
 
 class ACInfinityConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for AC Infinity."""
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_bluetooth(
+        self,
+        discovery_info: BluetoothServiceInfoBleak,
+    ):
 
-        errors = {}
+        address = discovery_info.address
+
+        await self.async_set_unique_id(address)
+        self._abort_if_unique_id_configured()
+
+        self.context["title_placeholders"] = {
+            "name": discovery_info.name or address
+        }
+
+        return await self.async_step_confirm()
+
+    async def async_step_confirm(self, user_input=None):
 
         if user_input is not None:
 
             return self.async_create_entry(
-                title=user_input[CONF_MAC],
-                data=user_input,
+                title=self.context["title_placeholders"]["name"],
+                data={"mac": self.unique_id},
             )
 
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_MAC): str,
-            }
-        )
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=schema,
-            errors=errors,
-        )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-
-        return ACInfinityOptionsFlow(config_entry)
-
-
-class ACInfinityOptionsFlow(config_entries.OptionsFlow):
-
-    def __init__(self, config_entry):
-
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-
-        return self.async_create_entry(title="", data={})
+        return self.async_show_form(step_id="confirm")
